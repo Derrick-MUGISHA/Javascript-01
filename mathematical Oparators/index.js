177efe5d938c098fd87df58e4a9251b95360fe93
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-03-11 17:34:20 +0200

    day 2 on javascript

---
+++ mathematical Oparators/index.js
@@ -0,0 +1,123 @@
+// // declaration of variables
+// let itemPrice = 25;
+// let tax = 5;
+
+
+
+// // addition
+// let total = itemPrice + tax;
+
+
+// console.log(total);
+// // subtraction
+// let total2 = itemPrice - tax;
+// total2 = itemPrice + tax; // update the declaration 
+// console.log(total2);
+
+
+// // division
+// let total3 = itemPrice / tax;
+// console.log(total3);
+
+
+// // multiplication
+// let total4 = itemPrice * tax;
+// console.log(total4);
+
+
+// // power
+// let total5 = itemPrice ** tax;
+// console.log(total5);
+
+// let event = 10 % 2;
+// console.log(event);
+
+
+
+// let flourWeight = 500;
+// let sugarWeight = 200;
+
+// a 
+
+// let totalWeight = flourWeight + sugarWeight;
+// console.log(totalWeight + " grams");
+
+// //b 
+
+// let dividedWeight = totalWeight / 3;
+// console.log(dividedWeight + " grams");
+
+
+// // c
+
+// let modulesWeight = dividedWeight % 3;
+// console.log(modulesWeight + " grams");
+
+
+
+// let bankBalance = 1000;
+
+
+// console.log(bankBalance + " " +'initial balance');
+
+
+// bankBalance += 500;
+// console.log(bankBalance + " "+'updated balance to be added on by 500');
+// bankBalance -= 150;
+// console.log(bankBalance + " "+'updated balance to be subtracted by 150');
+// bankBalance *= 2;
+// console.log(bankBalance + " "+'updated balance to be multiplied by 2');
+
+
+
+
+// let a = 22;
+// let b = "22";
+
+// const quality = a === b; // strictly equality operator checks the value and the data type  
+
+// const quality2 = a == b; // loose equality operator checks the value 
+
+// console.log(quality);
+
+
+
+
+// let userAge = 21;
+// console.log(userAge === 21);
+// console.log(userAge >=18);
+// console.log(userAge!== 13);
+
+
+// let x = 2
+// let y = "2"
+
+// console.log(x == y);  //
+// console.log(x != y);  // value 
+
+
+
+
+
+
+
+
+
+
+// let hasTicket = true;
+// let isVip = false;
+
+//  // and condition 
+// console.log(hasTicket && isVip);   //both conditions must be true 
+
+// // or condition
+// console.log(hasTicket || isVip);   // at least one condition must be true
+
+// let isNotLoggedIn = true;
+
+// console.log(!isNotLoggedIn);
+let cartTotal=50;
+let isPremiumMember  = true;
+let hasDiscountedCode = false;
+let isFraudelent=false;
+console.log(cartTotal > 100 || isPremiumMember  && !isFraudelent);
