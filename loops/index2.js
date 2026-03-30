8d20721b11a8b78e7f2040138e09741cd67c917f
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-03-30 17:12:21 +0200

    improved code base on arrays and objects

---
+++ loops/index2.js
@@ -0,0 +1,35 @@
+// const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'peach', 'quince', 'raspberry', 'strawberry', 'watermelon'];
+
+// let lengthFruits = fruits.length;
+
+// let text = "<ul>";
+
+// for(let i = 0; i < lengthFruits; i++) {
+//      text += "<li>" + fruits[i] + "</li>";
+// }
+
+// text += "</ul>";
+
+// // document.getElementById("demo").innerHTML = text;
+
+
+const fruits2 = ["Banana", "Orange", "Apple"];
+fruits2.push("Lemon"); 
+
+const type = fruits2 instanceof Array;
+console.log(type);
+
+
+
+// fruits2[fruits2.length] = "Kiwi";
+
+// console.log(fruits2);
+
+
+// const New = new Array("Banana");
+// console.log(New);
+
+
+// const points = new Array(40, 100, 1);
+
+// console.log(points);
